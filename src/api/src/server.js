5d833ab74af276c4b483c803ed2b307f04372632
Libre Lol <hello@libre.lol>
2024-11-18 15:33:05 -0500

    Made /api/v1 on the server side of things

--- src/api/src/server.js
+++ src/api/src/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+app.get('/api/v1/', (req, res) => {
     res.send('This insnt the frontend how did you get here!');
+});
+
+app.get('/api/v1/health', (req, res) => {
+    res.send('OK');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-app.get('/health', (req, res) => {
-    res.send('OK');
 });
