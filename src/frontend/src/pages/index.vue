4f8fc40c4fd14bb91f91d8ae2e232d0af94fa35e
Libre Lol <hello@libre.lol>
2024-11-18 15:46:12 -0500

    Added a landing page

--- src/frontend/src/pages/index.vue
+++ src/frontend/src/pages/index.vue
@@ -1,7 +1,22 @@
-<template>
-  <HelloWorld />
+</v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title class="headline">Direct Monero Update</v-card-title>
+          <v-card-text>
+            Direct Monero is being rewritten in its entirety. This is due to it used to be dependent on Vercel and MongoDB Atlas but we are switching to a secure self-ran service. So for now the platform will be closed. But feel free to check back and see our progress.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
 </script>
+
+<style scoped>
+.headline {
+  font-weight: bold;
+}
+</style>
